068178c24d92a3833e42fd66d593c21d564a2dd5
adarshchhetri9 <chhetriadarsh912@gmail.com>
2024-02-28 23:50:13 +0530

    till navbar

---
+++ public/nav/icons/index.ts
@@ -0,0 +1,5 @@
+import bestseller from "./bestsellers.jpg";
+import picks from "./picks.jpg";
+import nnews from "./new.jpg";
+
+export { bestseller, picks, nnews };
---
+++ public/nav/ui-kits/index.ts
@@ -0,0 +1,5 @@
+import blue from "./blue.jpg";
+import mixed from "./mixed.jpg";
+import purple from "./purple.jpg";
+
+export { blue, mixed, purple };
